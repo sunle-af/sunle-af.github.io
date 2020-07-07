071857ea396d683d57e1c5a25d9816acfdec8e7c
Subhash Suryawanshi <44070827+subhashsurya@users.noreply.github.com>
2020-07-07 20:28:18 +0530

    nearly done

--- js/index.js
+++ js/index.js
@@ -28,3 +28,8 @@ document.addEventListener('click', () => {
         cursor.classList.remove("expand");
     }, 500)
 })
+
+window.addEventListener("scroll",event=>{let scrollTop = document.documentElement.scrollTop;
+let scrollHeight = document.documentElement.scrollHeight-document.documentElement.clientHeight; 
+let progress =100*scrollTop/scrollHeight;
+document.querySelector(".progress").style.width=progress+"%";})
