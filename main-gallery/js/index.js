50a92eb6ce771f4cf2517b4c5e53d4fd9c858650
Subhash Suryawanshi <44070827+subhashsurya@users.noreply.github.com>
2020-07-11 17:35:21 +0530

    main-gallery

--- main-gallery/js/index.js
+++ main-gallery/js/index.js
@@ -27,3 +27,9 @@
   });
 
 }).call(this);
+window.addEventListener("scroll",event=>{let scrollTop = document.documentElement.scrollTop;
+  let scrollHeight = document.documentElement.scrollHeight-document.documentElement.clientHeight; 
+  let progress =100*scrollTop/scrollHeight;
+  document.querySelector(".progress").style.width=progress+"%";})
+
+ 
